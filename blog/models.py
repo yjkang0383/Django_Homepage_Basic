c74bf62769c77184337e84996c926c658976f2dc
Yoo Jeong <83561013+yjkang0383@users.noreply.github.com>
2022-02-11 15:58:41 +0900

    url, view

--- blog/models.py
+++ blog/models.py
@@ -9,11 +9,18 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    
+
+    #삭제 할 예정 마이그레이션 테스트 하기 위함
+    #test = models.TextField()
+
+    # Model 클래스에 정의된 __str__ 함수를 재정의(title 필드값을 리턴함)
+    def __str__(self):
+        return self.title+'('+ str(self.id)+')'
+
+
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = timezone.now() #글게시일자
         self.save()
         
-    def __str__(self):
-        return self.title
+
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    my_name = '장고웹프레임워크'
+    http_method = request.method
+    return HttpResponse('''
+        <h2> welcome {name} </h2>
+        <p> Http Method : {method}</p>
+        <p> Http headers : {header}</p>
+        <p> Http Path : {mypath} </p>
+    '''.format(name=my_name, method=http_method, header = request.headers['user-agent'], mypath = request.path))
+
