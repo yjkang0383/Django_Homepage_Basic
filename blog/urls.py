57276e0a92e7ae27a4bdccd9f072197301911090
Yoo Jeong <83561013+yjkang0383@users.noreply.github.com>
2022-02-17 17:34:28 +0900

    pagination

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     # http://localhost:8000/blog/post/1/remove
     path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
     # http://localhost:8000/blog/post/1/comment
-    path('post/<int:pk>/comment/', views.post_remove, name='add_comment_to_post'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    # http://localhost:8000/blog/comment/2/approve
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    # http://localhost:8000/blog/comment/2/remove
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
